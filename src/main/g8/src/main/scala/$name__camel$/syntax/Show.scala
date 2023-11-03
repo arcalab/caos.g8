b68857e9bc2c4edf818c64dbc00aa8a34f2f317a
José Proença <jose@proenca.org>
2023-11-03 08:39:21 +0000

    fixing $ templates

--- src/main/g8/src/main/scala/$name__camel$/syntax/Show.scala
+++ src/main/g8/src/main/scala/$name__camel$/syntax/Show.scala
@@ -1,7 +1,7 @@
-package $name;format="camel"$.syntax
+package \$name;format="camel"\$.syntax
 
-import $name;format="camel"$.syntax.Program.*
-import $name;format="camel"$.syntax.Program.Term.*
+import \$name;format="camel"\$.syntax.Program.*
+import \$name;format="camel"\$.syntax.Program.Term.*
 
 /**
  * List of functions to produce textual representations of commands
@@ -19,7 +19,7 @@ object Show:
   def apply(defs: Map[String,Term]): String =
     if defs.isEmpty then ""
     else "let\n"+
-      (for (p,t)<-defs yield s"  $p = ${apply(t)}").mkString("\n") +
+      (for (p,t)<-defs yield s"  \$p = \${apply(t)}").mkString("\n") +
       "\nin\n  "
 
   /** Pretty expression */
@@ -27,46 +27,46 @@ object Show:
     case Term.End => "0"
     case Term.Proc(p) => p
     case Term.Prefix(act, Term.End) => act
-    case Term.Prefix(act, t) => s"$act.${applyP(t)}"
-    case Term.Choice(t1, t2) => s"${applyP(t1)}+${applyP(t2)}"
-    case Term.Par(t1, t2) => s"${applyP(t1)} | ${applyP(t2)}"
+    case Term.Prefix(act, t) => s"\$act.\${applyP(t)}"
+    case Term.Choice(t1, t2) => s"\${applyP(t1)}+\${applyP(t2)}"
+    case Term.Par(t1, t2) => s"\${applyP(t1)} | \${applyP(t2)}"
 
   private def applyP(e:Term): String = e match
     case _:(End.type|Proc|Prefix) => apply(e)
-    case _ => s"(${apply(e)})"
+    case _ => s"(\${apply(e)})"
 
 
   /** Converts the main term into a mermaid diagram reflecting its structure. */
   def mermaid(s:System): String = "graph TD\n" +
-  s"  style ${s.main.hashCode()} fill:#ffe177,stroke:#6a6722,stroke-width:4px;\n" +
+  s"  style \${s.main.hashCode()} fill:#ffe177,stroke:#6a6722,stroke-width:4px;\n" +
     (term2merm(s.defs) ++
      term2merm(s.main)).mkString("\n")
 
       /** Builds nodes and arcs, using a set structure to avoid repetition. */
   private def term2merm(e: Term): Set[String] = e match
-    case Term.End => Set(s"  ${e.hashCode()}([\"0\"])")
-    case Term.Proc(p) => Set(s"  ${e.hashCode()}([\"$p\"])")
+    case Term.End => Set(s"  \${e.hashCode()}([\"0\"])")
+    case Term.Proc(p) => Set(s"  \${e.hashCode()}([\"\$p\"])")
     case Term.Prefix(act, t) => term2merm(t) ++
-      Set(s"  ${e.hashCode()} -->|action| ${act.hashCode()}",
-          s"  ${e.hashCode()} -->|rest| ${t.hashCode()}",
-          s"  ${e.hashCode()}([\"${apply(e)}\"])",
-          s"  ${act.hashCode()}([\"$act\"])"
+      Set(s"  \${e.hashCode()} -->|action| \${act.hashCode()}",
+          s"  \${e.hashCode()} -->|rest| \${t.hashCode()}",
+          s"  \${e.hashCode()}([\"\${apply(e)}\"])",
+          s"  \${act.hashCode()}([\"\$act\"])"
         )
     case Term.Choice(t1, t2) =>
       term2merm(t1) ++ term2merm(t2) ++
-      Set(s"  ${e.hashCode()} -->|option 1| ${t1.hashCode()}",
-          s"  ${e.hashCode()} -->|option 2| ${t2.hashCode()}",
-          s"  ${e.hashCode()}([\"${apply(e)}\"])")
+      Set(s"  \${e.hashCode()} -->|option 1| \${t1.hashCode()}",
+          s"  \${e.hashCode()} -->|option 2| \${t2.hashCode()}",
+          s"  \${e.hashCode()}([\"\${apply(e)}\"])")
     case Term.Par(t1, t2) =>
       term2merm(t1) ++ term2merm(t2) ++
-      Set(s"  ${e.hashCode()} -->|par 1| ${t1.hashCode()}",
-          s"  ${e.hashCode()} -->|par 2| ${t2.hashCode()}",
-          s"  ${e.hashCode()}([\"${apply(e)}\"])")
+      Set(s"  \${e.hashCode()} -->|par 1| \${t1.hashCode()}",
+          s"  \${e.hashCode()} -->|par 2| \${t2.hashCode()}",
+          s"  \${e.hashCode()}([\"\${apply(e)}\"])")
 
   /** Builds the nodes and arcs of the diagram of the definitions */
   private def term2merm(defs: Map[String,Term]): Set[String] = 
     defs.flatMap( (p,t) =>
       term2merm(t) +
-      s"  ${Proc(p).hashCode()}([\"$p\"])" +
-      s"  ${Proc(p).hashCode()} -->|definition| ${t.hashCode}"
+      s"  \${Proc(p).hashCode()}([\"\$p\"])" +
+      s"  \${Proc(p).hashCode()} -->|definition| \${t.hashCode}"
     ).toSet
